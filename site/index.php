8c48106089b7947af5099a99e6a499806a7d25c1
Nicolasvalade <nicolas@valade.mobi>
2022-02-21 23:23:05 +0100

    Routeur

--- site/index.php
+++ site/index.php
@@ -1,21 +1,21 @@
 <?php
 // gestion des routes
   $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-  if ('/index.php' == $uri)
+  $index='/FBN/site/index.php';
+  if ($index == $uri)
     {
-      require 'view/accueil.php';
+      require_once 'views/accueil.php';
     }
-    elseif ('/index.php/interventions' == $uri)
+    elseif ($index.'/interventions' == $uri)
     {
-      require 'controllers/interventions.php';
+      require_once 'controllers/interventions.php';
     }
-  elseif ('/index.php/intervention' == $uri && isset($_GET['id']))
+  elseif ($index.'/intervention' == $uri && isset($_GET['id']))
     {
-        require 'controllers/intervention.php';
+        require_once 'controllers/intervention.php';
     }
     else
     {
         header('Status: 404 Not Found');
-        echo '<html><body><h1>Page Not Found</h1></body></html>';
+        echo '<!DOCTYPE html><html><body><h1>'.$uri.' : page Not Found</h1></body></html>';
     }
-  ?>
